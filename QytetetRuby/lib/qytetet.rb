78cb55933f88751b3ae01f5c93d50c5f9389096e
robinloja97 <robinloja97@ei140175>
2018-09-24 19:03:43 +0200

    añadidas las clases tipo_sorpresa y qytetet, primera vez

---
+++ QytetetRuby/lib/qytetet.rb
@@ -0,0 +1,28 @@
+#encoding: utf­8
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+module ModeloQytetet
+  class Qytetet
+    attr_reader :mazo
+    
+    def initialize
+      @mazo=Array.new
+    end
+
+    def inicializar_cartas_sorpresa    
+      mazo<< Sorpresa.new("", 0, TipoSorpresa::SALIRCARCEL)
+      mazo<< Sorpresa.new("", 1, TipoSorpresa::PORJUGADOR)
+      mazo<< Sorpresa.new("", 2, TipoSorpresa::PORJUGADOR)
+      mazo<< Sorpresa.new("", 3, TipoSorpresa::PORCASAHOTEL)
+      mazo<< Sorpresa.new("", 4, TipoSorpresa::PORCASAHOTEL)
+      mazo<< Sorpresa.new("", 5, TipoSorpresa::IRACASILLA)
+      mazo<< Sorpresa.new("", 6, TipoSorpresa::IRACASILLA)
+      mazo<< Sorpresa.new("", 7, TipoSorpresa::IRACASILLA)
+      mazo<< Sorpresa.new("", 8, TipoSorpresa::PAGARCOBRAR)
+      mazo<< Sorpresa.new("", 0, TipoSorpresa::PAGARCOBRAR)
+    end
+    
+  end
+end
---
+++ QytetetRuby/lib/tipo_sorpresa.rb
@@ -0,0 +1,17 @@
+#encoding: utf­8
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+
+module ModeloQytetet
+  module TipoSorpresa
+  
+    PAGARCOBRAR = :Pagar_cobrar
+    IRACASILLA = :Ir_a_casilla
+    PORCASAHOTEL = :Por_casa_hotel
+    PORJUGADOR = :Por_jugador
+    SALIRCARCEL = :Salir_carcel
+
+  end
+end
