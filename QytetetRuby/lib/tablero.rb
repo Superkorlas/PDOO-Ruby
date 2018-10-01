3672796215ab8c291cd95032c63386e5cdd36a49
robin <robin@elBicho>
2018-10-01 19:14:04 +0200

    casillas especiales

--- QytetetRuby/lib/tablero.rb
+++ QytetetRuby/lib/tablero.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
@@ -15,11 +16,38 @@ module ModeloQytetet
     def inicializar_tablero
       @casillas=Array.new
       
+      
+      
       #Rellenar las demas, ver Tablero.java
-      casilla_salida=Casilla.new(0, TipoCasilla::SALIDA)
+      casilla_salida    =Casilla.new(0, TipoCasilla::SALIDA)
       @casillas << casilla_salida
-      @carcel=Casilla.new(1,TipoCasilla::CARCEL)
-      @carcel.to_s
+      
+      titulo1 = Titulo.new(nombre, precioCompra, alquilerBase, factorRevalorizacion, hipotecaBase, precioEdificar)
+      casilla_calle1
+      @casillas
+            
+      casilla_carcel    =Casilla.new(5, TipoCasilla::CARCEL)
+      @casillas << casilla_carcel
+      
+      casilla_sorpresa1 =Casilla.new(7, TipoCasilla::SORPRESA)
+      @casillas << casilla_sorpresa1
+      
+      casilla_parking   =Casilla.new(10, TipoCasilla::PARKING)
+      @casillas << casilla_parking
+      
+      casilla_impuesto  =Casilla.new(12, TipoCasilla::IMPUESTO)
+      @casillas << casilla_impuesto
+      
+      casilla_sorpresa2 =Casilla.new(13, TipoCasilla::SORPRESA)
+      @casillas << casilla_sorpresa2
+      
+      casilla_juez      =Casilla.new(15, TipoCasilla::JUEZ)
+      @casillas << casilla_juez
+      
+      casilla_sorpresa3 =Casilla.new(18, TipoCasilla::SORPRESA)
+      @casillas << casilla_sorpresa3  
+                
+      @carcel=casilla_carcel
             
     end
       
